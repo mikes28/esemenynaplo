5c7477110a058a89fcadae3a64a82c444069cc79
mikes28 <mikeskrisztian06@gmail.com>
2025-11-26 12:05:47 +0100

    made the form to enter new entries using the addEvent() function. renamed the previous console const to cli, so console.log is working.

--- esemenynaplo/src/main.js
+++ esemenynaplo/src/main.js
@@ -1,7 +1,7 @@
 import SystemEvent from "./SystemEvent";
 
-const console = document.getElementById("console");
-const events = [
+const cli = document.getElementById("console");
+let events = [
   new SystemEvent("info", "Rendszer indítása", Date.now()),
   new SystemEvent("warn", "Magas memóriahasználat", Date.now()),
   new SystemEvent("error", "Adatbázis kapcsolat megszakadt", Date.now()),
@@ -9,12 +9,21 @@ const events = [
   new SystemEvent("warn", "CPU terhelés kritikus szinten", Date.now()),
 ];
 function listEvents() {
+  cli.innerHTML="";
   events.forEach(e => {
 
     let eventLine = document.createElement("div");
     eventLine.innerText += `${e} \n`;
     eventLine.classList=e.type;
-    console.appendChild(eventLine);
+    cli.appendChild(eventLine);
   });
 }
-document.addEventListener("DOMContentLoaded", listEvents());
+
+function addEvent(){
+  events.push(new SystemEvent( document.getElementById("eventType").value,
+  document.getElementById("event").value, new Date()));
+  listEvents();
+}
+document.getElementById("addEntryB").addEventListener("click", addEvent);
+document.addEventListener("DOMContentLoaded", listEvents);
+
