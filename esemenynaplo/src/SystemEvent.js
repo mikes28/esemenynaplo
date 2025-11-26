84968ddf2ef677d4c2b516e54c28a48306a0315c
mikes28 <mikeskrisztian06@gmail.com>
2025-11-26 11:18:59 +0100

    created SystemEvent class

---
+++ esemenynaplo/src/SystemEvent.js
@@ -0,0 +1,8 @@
+export default class SystemEvent {
+    constructor(type, desc, time) {
+        this.username = username;
+        this.time = new Date(time);
+        this.success = success;
+    }
+    
+}
